4448bf924ecac00e5869a09617bbbf9900d03940
Hossein Gholami <hosseingholami.1995@outlook.com>
2020-03-24 00:04:33 +0430

    play again page

--- 15puzzel.c
+++ 15puzzel.c
@@ -1,10 +1,16 @@
-//optimize the boolian value
-#define ERROR -1
-#define EASY 0
-#define NORMAL 1
-#define HARD 2
+//optimize the boolian value could 
+
+#define True 1
+#define False 0
+
+#define EASY 10
+#define NORMAL 20
+#define HARD 30
+#define ERROR 40
+
 #define Esc 27
 
+#include<time.h>//for random
 #include <stdio.h>
 #include <conio.h>//for getch
 
@@ -13,48 +19,106 @@ void show_table(unsigned char x[4][4] );
 unsigned char MoveRule(unsigned char x[4][4],unsigned char input,unsigned char *X,unsigned char *Y);
 unsigned char show_welcome_page();
 void random_move(unsigned char NUM,unsigned char x[4][4],unsigned char *X,unsigned char *Y);
-
+unsigned char CheckWin(unsigned char x[4][4]);
+void shuffle_table(unsigned char lvl,unsigned char x[4][4],unsigned char *X,unsigned char *Y);
+unsigned char show_play_again_page();	
 
 void main(){
+//Reserv and initial parameter on RAM
 	unsigned char i,j;//used in for loop
-	unsigned char x[4][4],winer_pattern[4][4];//used as number in puzzel
-	unsigned char exit=0; //use as boolian value 
+	unsigned char x[4][4];//used as number in puzzel
+	unsigned char exit=False,win=False,result=False; //use as boolian value 
 	unsigned char X=3; //use as position of +
 	unsigned char Y=3; //use as position of +
 	unsigned char lvl;//use as hardness lvl
 	unsigned char input;//use as keybord read 
-
+//initial the game
 	init(x);
 	lvl=show_welcome_page();
-//shuffle the table about 
+	shuffle_table(lvl,x,&X,&Y);
+	
+	//game started 
+	do {
+		system("cls");
+		show_table(x);
+
+		if(win==False){
+						input=getch();
+			if(input==Esc)exit=True;
+			MoveRule(x,input,&X,&Y);
+			if(CheckWin(x))win=True;
+		}
+		else{
+			result=show_play_again_page();
+			if (result){
+				lvl=show_welcome_page();
+				shuffle_table(lvl,x,&X,&Y);
+				win=False;
+			}else exit =True;
+		}
+	}while(exit==False);
+
+}
+
+//show welcom page and lvl choos
+unsigned char show_play_again_page(){
+	system("cls");
+	printf("  		/\\________________	15 Puzzel Game		________________/\\ \n");
+	printf("		==================================================================\n");
+	printf("		||								||\n");
+	printf("		||congratulations,Do you whant play again?	 		||\n");
+	printf("		||								||\n");
+	printf("		||								||\n");
+	printf("		||								||\n");
+	printf("		||	press y to play again  					||\n");
+	printf("		||	or press any thing else to quit				||\n");
+	printf("		||								||\n");
+	printf("		||								||\n");
+	printf("		||								||\n");
+	printf("		||								||\n");
+	printf("		||								||\n");
+	printf("		||								||\n");
+	printf("		==================================================================\n");
+	printf("			");
+	unsigned char input;
+	input=getch();
+	if((input=='y')||(input=='Y'))
+		return True;
+	else 
+		return False;
+
+}
+//check it if its on win pattern
+unsigned char CheckWin(unsigned char x[4][4]){
+	unsigned char WinnerPattern[4][4],i,j,equal;
+	equal=1;
+	init(WinnerPattern);
+	for(i=0;i<4;i++)
+		for(j=0;j<4;j++)
+			if(WinnerPattern[i][j]!=x[i][j])
+				equal=0;
+	return equal;
+}
+//shuffle
+void shuffle_table(unsigned char lvl,unsigned char x[4][4],unsigned char *X,unsigned char *Y){
 	switch (lvl){
 		case EASY:
-				random_move(3,x,&X,&Y);
+				random_move(3,x,X,Y);
 			break;
 		case NORMAL:
-				random_move(30,x,&X,&Y);
+				random_move(30,x,X,Y);
 			break;
-		
 		case HARD:
-				random_move(200,x,&X,&Y);
+				random_move(200,x,X,Y);
 			break;
 		default:
 		break;	
 		}
-	
-	
-	do {
-		system("cls");
-		show_table(x);
-		input=getch();
-		if(input==Esc)exit=1;
-		MoveRule(x,input,&X,&Y);
-	}while(!exit);
-		
 
-}
+	}
 //level implement via random move
 void random_move(unsigned char NUM,unsigned char x[4][4],unsigned char *X,unsigned char *Y){
+	 srand((unsigned) time(NULL));
 	unsigned char i,input;
 	for(i=0;i<NUM;i++){
 		switch(rand()%4){
@@ -73,7 +137,7 @@ void random_move(unsigned char NUM,unsigned char x[4][4],unsigned char *X,unsign
 		}
 		MoveRule(x,input,X,Y);
 	}
-	MoveRule(x,'a',X,Y);
+	MoveRule(x,'d',X,Y);
 }
 //show welcom page and lvl choos
 unsigned char show_welcome_page(){
@@ -210,6 +274,6 @@ void show_table(unsigned char x[4][4] ){
 			}
 	}
 	printf("		==================================================================\n");
-	printf("  		\\/________________	Hossein Gholami		________________\\/ \n");
+	printf("  		\\/________________	press \"Esc\" to quit	________________\\/ \n");
 
 }
